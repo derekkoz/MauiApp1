f2c49127b50c4c5b4a29672d92f838ff98371861
derek kozlowski <derekkozlowski@Mac-Studio.local>
2025-12-06 08:08:50 -0500

    Make ToDoItem tolerant for SQL binding JSON deserialization

--- ToDoItem.cs
+++ ToDoItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json.Serialization;
 
 namespace AzureSQL.ToDo;
@@ -6,9 +7,17 @@ public class ToDoItem
 {
     [JsonPropertyName("id")]
     public Guid Id { get; set; }
+
+    [JsonPropertyName("order")]
     public int? order { get; set; }
-    public required string title { get; set; }
-    public required string url { get; set; }
+
+    [JsonPropertyName("title")]
+    public string title { get; set; } = string.Empty;
+
+    [JsonPropertyName("url")]
+    public string url { get; set; } = string.Empty;
+
+    [JsonPropertyName("completed")]
     public bool? completed { get; set; }
 }
 
